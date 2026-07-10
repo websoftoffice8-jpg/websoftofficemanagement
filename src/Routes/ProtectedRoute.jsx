debf991a5b886b085579a23cb5febb2743fa9269
Gaurav <GauravDhungana322@gmail.com>
2026-07-10 10:54:13 +0545

    package installation

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,23 +1,24 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, role }) => {
-  const user = JSON.parse(localStorage.getItem("user"));
+    const user = JSON.parse(localStorage.getItem("user"));
 
-  // Not logged in
-  if (!user) {
-    return <Navigate to="/" replace />;
-  }
-
-  // Wrong role
-  if (role && user.role !== role) {
-    if (user.role === "admin") {
-      return <Navigate to="/admin/dashboard" replace />;
+    // Not logged in
+    if (!user) {
+        return <Navigate to="/" replace />;
     }
 
-    return <Navigate to="/employee/dashboard" replace />;
-  }
+    // Wrong role
+    if (role && user.role !== role) {
+        if (user.role === "admin") {
+            return <Navigate to="/admin/dashboard" replace />;
+        }
+       
 
-  return children;
+
+    }
+
+    return children;
 };
 
 export default ProtectedRoute;
