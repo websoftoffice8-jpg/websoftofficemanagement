8091e19deae8ddd9bb94170e79b6fcf018fdba9d
Prashant01-gif <imprashantsenn@gmail.com>
2026-07-27 12:11:37 +0545

    new BAr chart

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2000/",
+  baseURL: "http://192.168.1.87:2000/",
   headers: {
     "Content-Type": "application/json",
   },
